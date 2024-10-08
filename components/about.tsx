fb8045c2f6a3a2753d4c459174322bffa7d58e8f
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-10-08 09:51:26 -0400

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -19,18 +19,17 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-      A <span className="font-medium">Redis certified Software Developer, Microsoft certified AI Engineer</span> {" "} and <span className="font-medium"> Data Scientist </span> with 2+ years of professional experience in optimizing microservices, web applications and 3+ years of project experience on building AI, Machine Learning, and Spring Boot applications.
+      A <span className="font-medium">Redis certified Software Developer, certified AI Engineer</span> {" "} and <span className="font-medium"> Data Scientist </span> with 3+ years of professional experience in designing, developing, automating, testing web applications and 3+ years of project experience on building end-to-end AI, Machine Learning, Deep Learning web applications.
       </p>
       <p className="mb-3">
       I am a recent <span className="font-medium">computer science</span> graduate student with a GPA of 4.0 from the University of South Florida (2022-2024).
       </p>
       <p className="mb-3">
-      During my tenure as a <span className="font-medium">Software Engineer</span> at <span className="font-medium">Cognizant</span> I've contributed to a major US pharmaceutical company's software development, reducing defects by 30% and 
+      During my tenure as a <span className="font-medium">Software Engineer</span> at <span className="font-medium">Cognizant</span> I've contributed to a major U.S.-based manufacturing company's software development, reducing defects by 30% and 
       proven ability to accelerate data access, enhance front-end performance, and increase testing coverage.
       </p>
       <p className="mb-3">
-      I am now actively looking for full-time opportunities and eager to apply my extensive experience and skills in a new role, contributing effectively to your organization's success.
-      </p>
+      I am now actively seeking full-time opportunities to apply my skills in developing secure, scalable, reliable and highly performant applications.</p>
     </motion.section>
   );
 }
